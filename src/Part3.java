d8d58a0fef3afee456f55e9154e06967e71992c3
Aevismotax1 <108089675+AevisMotax@users.noreply.github.com>
2023-05-10 15:34:26 -0400

    Update Part3.java

--- src/Part3.java
+++ src/Part3.java
@@ -1,11 +1,3 @@
-/*
- * ---------------------------
- * Assignment 3   
- * Written by: Robert CHEN 40241709 and Alexandru Ilie 40248696
- * COMP 249 Section S - Winter 2023
- * 27th march  2023
- * --------------------------- */
-
 import java.io.FileInputStream;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
